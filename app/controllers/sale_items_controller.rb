d0bb67dafbd306b312f4d943bbc552b02d136579
Glenn Goodrich <glenn.goodrich@gmail.com>
2010-08-06 17:03:54 -0400

    Add the habtm for children

--- app/controllers/sale_items_controller.rb
+++ app/controllers/sale_items_controller.rb
@@ -1,4 +1,4 @@
-class SaleItemsController < ActionController::Base
+class SaleItemsController < ApplicationController
   
   def index
     @sale_items = SaleItem.all
@@ -15,6 +15,51 @@ class SaleItemsController < ActionController::Base
   
   def edit
     @sale_item = SaleItem.find(params[:id])
+    
+  end
+  
+  def create
+    @sale_item = SaleItem.new(params[:sale_item])
+    
+    respond_to do |format|
+      if @sale_item.save
+        format.html {redirect_to sale_items_path, :notice => "Sale item successfully created"}
+        format.xml {render :xml => @sale_item, :status=>:created, :location=>@sale_item}
+      else
+        format.html { render :new}
+        format.xml {render :xml => @sale_item.errors, :status=>:unprocessible_entity}
+      end
+    end
+  end
+  
+  def update
+    @sale_item = SaleItem.find(params[:id])
+    
+    respond_to do |format|
+      if @sale_item.update_attributes(params[:sale_item])
+        format.html { redirect_to sale_items_path, :notice => "Sale item successfully modified"}
+        format.xml { head :ok }
+      else
+        format.html { render :action => 'edit'}
+        format.xml {render :xml => @sale_item.errors, :status=>:unprocessible_entity}
+      end   
+    end
+    
+  end
+  
+  def destroy
+    @sale_item = SaleItem.find(params[:id])
+    
+    respond_to do |format|
+      if @sale_item.destroy
+        format.html {redirect_to sale_items_path, :notice=>"Sale item successfully deleted"}
+        format.xml { head :ok}
+      else
+        format.html {redirect_to sale_items_path, :notice=>"Sale item NOT deleted"}
+        format.xml { head :bad_request}
+      end
+    end
+    
     
   end
   
--- app/models/person.rb
+++ app/models/person.rb
@@ -2,6 +2,10 @@ class Person < ActiveRecord::Base
   set_table_name "ThornhillPerson"
   belongs_to :address, :foreign_key => "AddressID"
   has_many :emails, :foreign_key=>"PersonID"
+  has_and_belongs_to_many :children, :class_name => "Person",
+  :join_table => "ThornhillChild", :association_foreign_key => "ChildID", :foreign_key => "ParentID"
+
+  
   def full_name
     [self.FirstName, self.LastName].join(' ')
   end
